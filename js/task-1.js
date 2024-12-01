46915fca80042e7ea85b9fc2c1d9788e93a94012
Olena Prosiatnikova <margulay1976@gmail.com>
2024-12-01 16:26:48 +0200

    Task 1. Ordering droids

---
+++ js/task-1.js
@@ -0,0 +1,28 @@
+
+
+
+//Оголоси функцію makeTransaction, яка очікує два параметри,
+ //   значення яких будуть задаватися під час її виклику:
+ //• quantity— перший параметр, число, що містить кількість замовлених дроїдів 
+ //• pricePerDroid — другий параметр, число, що містить вартість одного дроїда
+
+
+
+//Доповни код функції так, щоб вона повертала рядок з повідомленням про покупку ремонтних дроїдів:
+//"You ordered <quantity> droids worth <totalPrice> credits!", де: 
+//• <quantity> — це кількість замовлених дроїдів
+// • <totalPrice> — це загальна вартість замовлення, тобто вартість усіх замовлених дроїдів
+
+      //  Візьми код нижче і встав після оголошення своєї функції для перевірки коректності
+        //</totalPrice>її роботи.
+      //</quantity>  У консоль будуть виведені результати її роботи.
+
+function makeTransaction(quantity, pricePerDroid) {
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+}
+makeTransaction(quantity, pricePerDroid)
+
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
