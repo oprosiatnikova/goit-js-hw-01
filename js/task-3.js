3949eec68824595ee73899b75c8310694c6e92d7
Olena Prosiatnikova <margulay1976@gmail.com>
2024-12-08 17:12:13 +0200

    add output

--- js/task-3.js
+++ js/task-3.js
@@ -20,7 +20,6 @@ const paddingWidth = parseFloat(padding);
     return boxSizing;
 }
 
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
-
+console.log(getElementWidth("50px", "8px", "4px")); 
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px"));
